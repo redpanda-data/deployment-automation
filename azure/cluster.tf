cdc411fe7260ddf33747caee77f47d904e2d7ba6
James Kinley <72576466+jrkinley@users.noreply.github.com>
2021-11-12 19:03:35 +0000

    Azure Terraform Configuration (#44)
    * Azure Terraform configuration
    
    * Update README.md
    
    * Ignore ansible hosts file
    
    * Remove custom DNS servers
    
    * VM details for ansible
    
    * Data disk format and mount
    
    * New variables
    
    * Monitoring VM config
    
    * Write Ansible inventory
    
    * Add monitor host to ansible inventory
    
    * Fix hosts template for Azure
    
    * Azure proximity placement group

---
+++ azure/cluster.tf
@@ -0,0 +1,138 @@
+#
+# Redpanda broker VMs
+#
+
+resource "azurerm_linux_virtual_machine" "redpanda" {
+  name                         = "redpanda_broker${count.index}"
+  computer_name                = "redpanda${count.index}"
+  count                        = var.vm_instances
+  resource_group_name          = azurerm_resource_group.redpanda.name
+  location                     = azurerm_resource_group.redpanda.location
+  availability_set_id          = azurerm_availability_set.redpanda.id
+  proximity_placement_group_id = azurerm_proximity_placement_group.redpanda.id
+  zone                         = try(var.zone, null)
+  size                         = var.vm_sku
+  admin_username               = var.admin_username
+  network_interface_ids        = ["${element(azurerm_network_interface.redpanda.*.id, count.index)}"]
+  
+  os_disk {
+    storage_account_type = "Premium_LRS"
+    caching              = "ReadWrite"
+  }
+  
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key)
+  }
+
+  source_image_reference {
+    publisher = var.vm_image.publisher
+    offer     = var.vm_image.offer
+    sku       = var.vm_image.sku
+    version   = var.vm_image.version
+  }
+
+  tags = {
+    deployment_id = local.deployment_id
+  }
+}
+
+resource "azurerm_managed_disk" "redpanda" {
+  name                 = "managed_disk${count.index}"
+  count                = var.vm_add_data_disk ? var.vm_instances : 0
+  resource_group_name  = azurerm_resource_group.redpanda.name
+  location             = azurerm_resource_group.redpanda.location
+  storage_account_type = "Premium_LRS"
+  create_option        = "Empty"
+  # https://docs.microsoft.com/en-us/azure/virtual-machines/disks-change-performance
+  disk_size_gb         = var.vm_data_disk_gb
+
+  tags = {
+    deployment_id = local.deployment_id
+  }
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "redpanda" {
+  count                     = var.vm_add_data_disk ? var.vm_instances : 0
+  managed_disk_id           = "${element(azurerm_managed_disk.redpanda.*.id, count.index)}"
+  virtual_machine_id        = "${element(azurerm_linux_virtual_machine.redpanda.*.id, count.index)}"
+  lun                       = "10"
+  caching                   = "None"
+  # Only available on M-series with Premium_LRS disks and no caching:
+  write_accelerator_enabled = false
+}
+
+#
+# Client VMs
+#
+
+resource "azurerm_linux_virtual_machine" "redpanda_client" {
+  name                         = "redpanda_client${count.index}"
+  computer_name                = "client${count.index}"
+  count                        = var.client_vm_instances
+  resource_group_name          = azurerm_resource_group.redpanda.name
+  location                     = azurerm_resource_group.redpanda.location
+  availability_set_id          = azurerm_availability_set.redpanda.id
+  proximity_placement_group_id = azurerm_proximity_placement_group.redpanda.id
+  size                         = var.client_vm_sku
+  admin_username               = var.admin_username
+  network_interface_ids        = ["${element(azurerm_network_interface.redpanda_client.*.id, count.index)}"]
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key)
+  }
+
+  source_image_reference {
+    publisher = var.vm_image.publisher
+    offer     = var.vm_image.offer
+    sku       = var.vm_image.sku
+    version   = var.vm_image.version
+  }
+
+  tags = {
+    deployment_id = local.deployment_id
+  }
+}
+
+#
+# Monitoring VM
+#
+
+resource "azurerm_linux_virtual_machine" "monitoring" {
+  name                         = "monitor"
+  count                        = var.enable_monitoring ? 1 : 0
+  resource_group_name          = azurerm_resource_group.redpanda.name
+  location                     = azurerm_resource_group.redpanda.location
+  availability_set_id          = azurerm_availability_set.redpanda.id
+  proximity_placement_group_id = azurerm_proximity_placement_group.redpanda.id
+  size                         = var.monitoring_vm_sku
+  admin_username               = var.admin_username
+  network_interface_ids        = ["${element(azurerm_network_interface.monitoring.*.id, count.index)}"]
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key)
+  }
+
+  source_image_reference {
+    publisher = var.vm_image.publisher
+    offer     = var.vm_image.offer
+    sku       = var.vm_image.sku
+    version   = var.vm_image.version
+  }
+
+  tags = {
+    deployment_id = local.deployment_id
+  }
+}
---
+++ azure/vars.tf
@@ -0,0 +1,84 @@
+variable "region" {
+  description = "Azure Region where the Resource Group will exist"
+  default     = "North Europe"
+}
+
+variable "zone" {
+  description = "Availability Zone"
+  default     = null
+}
+
+variable "vm_sku" {
+  description = "Azure VM SKU to use for the Redpanda nodes"
+  default     = "Standard_L32s_v2" # Lsv2-series sizes have local NVMe disks
+}
+
+variable "vm_instances" {
+  description = "Number of Redpanda nodes to create"
+  type        = number
+  default     = 3
+}
+
+variable "vm_add_data_disk" {
+  description = "Attach a Premium_LRS data disk to each node?"
+  type        = bool
+  default     = false
+}
+
+variable "vm_data_disk_gb" {
+  description = "Size of the Premium_LRS data disk in GiB"
+  type        = number
+  default     = 2048 #P50
+}
+
+variable "client_vm_sku" {
+  description = "Azure VM SKU to use for the client node"
+  default     = "Standard_D4ds_v5"
+  # Note when benchmark testing to match the max network
+  # bandwidth with the Redpanda nodes.
+}
+
+variable "client_vm_instances" {
+  description = "Number of client nodes to create"
+  type        = number
+  default     = 1
+}
+
+variable "enable_monitoring" {
+  description = "Setup a Prometheus/Grafana instance?"
+  type        = bool
+  default     = true
+}
+
+variable "monitoring_vm_sku" {
+  description = "Azure VM SKU to use for the monitoring node"
+  default     = "Standard_D2ds_v5"
+}
+
+variable "vm_image" {
+  description = "Source image reference for the VMs"
+  type = object({
+    publisher = string
+    offer     = string
+    sku       = string
+    version   = string
+  })
+  # Ubuntu 20.04 LTS
+  # https://github.com/Azure/azure-cli/issues/13320#issuecomment-649867249
+  default = {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  } 
+}
+
+variable "admin_username" {
+  description = "Username for the local administrator on each VM"
+  default     = "adminpanda"
+}
+
+variable "public_key" {
+  description = "Public Key file used for authentication"
+  default     = "~/.ssh/id_rsa.pub"
+}
