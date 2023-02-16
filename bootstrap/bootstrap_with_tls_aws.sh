ca0efc40b29850ce4f8081c7ccb5923476f13e5a
gene-redpanda <123959009+gene-redpanda@users.noreply.github.com>
2023-02-16 14:35:46 -0500

    bootstrap script fixes

--- bootstrap/bootstrap_with_tls_aws.sh
+++ bootstrap/bootstrap_with_tls_aws.sh
@@ -2,12 +2,12 @@ cd ../aws || exit
 
 ## env vars
 
-# get these from AWS directly or provide your own auth method for terraform
+## get these from AWS directly or provide your own auth method for terraform
 export AWS_ACCESS_KEY_ID=KEY
 export AWS_SECRET_ACCESS_KEY=KEY
 export AWS_SESSION_TOKEN=KEY
 
-# path to the ssh private key you are using to log into nodes
+## path to the ssh private key you are using to log into nodes
 export SSH_KEY_LOC=KEY
 
 ## terraform
@@ -48,15 +48,12 @@ ansible-playbook --private-key $SSH_KEY_LOC -i hosts.ini -v ansible/playbooks/in
 echo "done"
 
 # parse hosts.ini public ips into a redpanda_brokers compatible format for public and private ips for testing
-echo "REDPANDA_BROKERS=$(awk '/\[redpanda\]/{a=1;next}/\[monitor\]/{a=0}a{if($1~/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $1}' < hosts.ini | sed 's/$/:9093/' | tr '\n' ',' | sed 's/,$/\n/')"
+echo "REDPANDA_BROKERS=$(awk '/\[redpanda\]/{a=1;next}/\[monitor\]/{a=0}a{if($1~/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $1}' < hosts.ini | sed 's/$/:9092/' | tr '\n' ',' | sed 's/,$/\n/')"
 echo "REDPANDA_BROKERS=$(sed -n '/^\[redpanda\]/, /^\[monitor\]/p' hosts.ini | grep 'private_ip=' | cut -d= -f4 | sed 's/$/:9092/' | tr '\n' ',' | sed 's/,$/\n/')"
 
 # export the brokers for use in automated test
-REDPANDA_BROKERS=$(awk '/\[redpanda\]/{a=1;next}/\[monitor\]/{a=0}a{if($1~/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $1}' < hosts.ini | sed 's/$/:9093/' | tr '\n' ',' | sed 's/,$/\n/')
+REDPANDA_BROKERS=$(awk '/\[redpanda\]/{a=1;next}/\[monitor\]/{a=0}a{if($1~/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $1}' < hosts.ini | sed 's/$/:9092/' | tr '\n' ',' | sed 's/,$/\n/')
 export REDPANDA_BROKERS
-
-# generate ssh commands for testing (assumes we are using us-west-2)
-awk '/\[redpanda\]/{a=1;next}/\[monitor\]/{a=0}a{if($1~/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $1}' < hosts.ini | while read -r ip_address; do tmp_ip=$(echo "$ip_address" | tr '.' '-'); echo "ssh -i ${SSH_KEY_LOC} ubuntu@ec2-${tmp_ip}.us-west-2.compute.amazonaws.com"; done
 
 # you will need the ca.crt to use these commands on the client inside the VPC and from your workstation, but not on the nodes
 rpk cluster status \
@@ -69,3 +66,5 @@ rpk topic create testtopic2      \
 --tls-truststore ansible/playbooks/tls/ca/ca.crt \
 -v
 
+# generate ssh commands for testing (assumes we are using us-west-2)
+awk '/\[redpanda\]/{a=1;next}/\[monitor\]/{a=0}a{if($1~/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $1}' < hosts.ini | while read -r ip_address; do tmp_ip=$(echo "$ip_address" | tr '.' '-'); echo "ssh -i ${SSH_KEY_LOC} ubuntu@ec2-${tmp_ip}.us-west-2.compute.amazonaws.com"; done
