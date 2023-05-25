c323c68565583c54a2ddb6d2bf72f6e604962c68
Paul Wilkinson <pmw@redpanda.com>
2023-05-25 13:56:02 +0100

    Adding private network capability to Azure.

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -1,5 +1,5 @@
 output "redpanda_public_ips" {
-  value = "${azurerm_public_ip.redpanda.*.ip_address}"
+  value = var.vm_public_networking ? "${azurerm_public_ip.redpanda.*.ip_address}" : "${azurerm_network_interface.redpanda.*.private_ip_address}"
 }
 
 output "redpanda_private_ips" {
@@ -7,7 +7,7 @@ output "redpanda_private_ips" {
 }
 
 output "client_public_ips" {
-  value = "${azurerm_public_ip.redpanda_client.*.ip_address}"
+  value = var.client_vm_public_networking ? "${azurerm_public_ip.redpanda_client.*.ip_address}" : "${azurerm_network_interface.redpanda_client.*.private_ip_address}"
 }
 
 output "client_private_ips" {
@@ -15,7 +15,7 @@ output "client_private_ips" {
 }
 
 output "monitor_public_ip" {
-  value = "${azurerm_public_ip.monitoring.*.ip_address}"
+  value = var.monitoring_vm_public_networking ? "${azurerm_public_ip.monitoring.*.ip_address}" : "${azurerm_network_interface.monitoring.*.private_ip_address}"
 }
 
 output "monitor_private_ip" {
@@ -33,11 +33,11 @@ output "public_key_path" {
 resource "local_file" "hosts_ini" {
   content = templatefile("${path.module}/../templates/hosts_ini.tpl",
     {
-      redpanda_public_ips        = "${azurerm_public_ip.redpanda.*.ip_address}"
+      redpanda_public_ips        = var.vm_public_networking ? "${azurerm_public_ip.redpanda.*.ip_address}" : "${azurerm_network_interface.redpanda.*.private_ip_address}"
       redpanda_private_ips       = "${azurerm_network_interface.redpanda.*.private_ip_address}"
-      client_public_ips          = "${azurerm_public_ip.redpanda_client.*.ip_address}"
+      client_public_ips          = var.client_vm_public_networking ? "${azurerm_public_ip.redpanda_client.*.ip_address}" : "${azurerm_network_interface.redpanda_client.*.private_ip_address}"
       client_private_ips         = "${azurerm_network_interface.redpanda_client.*.private_ip_address}"
-      monitor_public_ip          = var.enable_monitoring ? "${azurerm_public_ip.monitoring.0.ip_address}" : ""
+      monitor_public_ip          = var.enable_monitoring ? (var.monitoring_vm_public_networking ? "${element(azurerm_public_ip.monitoring.*.ip_address, 0)}" : "${element(azurerm_network_interface.monitoring.*.private_ip_address, 0)}") : ""
       monitor_private_ip         = var.enable_monitoring ? "${azurerm_network_interface.monitoring.0.private_ip_address}" : ""
       enable_monitoring          = "${var.enable_monitoring}"
       ssh_user                   = "${var.admin_username}"
